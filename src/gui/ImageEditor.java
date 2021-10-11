7e27b98b4fdc17bd498f089ddb1b2d8c77e2ce18
Rey Dominic Tome <70247995+reydominictome@users.noreply.github.com>
2021-10-11 16:22:27 +0800

    Pushed Photobooth

---
+++ src/gui/ImageEditor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+/**
+ *
+ * @author Rey
+ */
+public class ImageEditor {
+    public static void toGreyScale(String file){
+        
+    }
+}
